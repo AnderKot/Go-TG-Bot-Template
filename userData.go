ba10f85bc21ee1699ce7f8c2eff61da22003a5db
Ander_kot <andrei2297@gmail.com>
2024-02-16 00:28:09 +0300

    You don't see that ))

--- userData.go
+++ userData.go
@@ -6,13 +6,4 @@ type userData struct {
 	firstName    string
 	lastName     string
 	languageСode string
-	// Статистика
-	totalMatches  int
-	winPercentage float32
-	// Баланс
-	walletBalanse  float32
-	walletCurrency string
-	// Рефералы
-	referralsCount int
-	referralsCash  float32
 }
